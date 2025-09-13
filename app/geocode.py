57842dabb89d10ac4fe54b449e3c95378312da4f
Irsa Ashraf <irsaashraf@irsas-mbp.mynetworksettings.com>
2025-09-12 21:21:18 -0400

    Changed function names

--- app/geocode.py
+++ app/geocode.py
@@ -6,13 +6,13 @@ _geolocator = Nominatim(user_agent=GEOCODER_USER_AGENT)
 _forward = RateLimiter(_geolocator.geocode, min_delay_seconds=GEOCODER_MIN_DELAY_SEC)
 _reverse = RateLimiter(_geolocator.reverse, min_delay_seconds=GEOCODER_MIN_DELAY_SEC)
 
-def forward(address: str):
+def geocode_forward(address: str):
     loc = _forward(address)
     if not loc:
         return None
     return {"lat": loc.latitude, "lon": loc.longitude, "display_name": loc.address}
 
-def reverse(lat: float, lon: float):
+def geocode_reverse(lat: float, lon: float):
     loc = _reverse((lat, lon), language="en")
     if not loc:
         return None
